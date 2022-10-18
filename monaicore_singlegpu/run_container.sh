1fc45a66871fc10d30b27f18c6ac4c7e0fd9b47e
hw-ju <90806807+hw-ju@users.noreply.github.com>
2022-10-17 17:19:48 -0700

    Update run_container.sh

--- monaicore_singlegpu/run_container.sh
+++ monaicore_singlegpu/run_container.sh
@@ -13,7 +13,7 @@ date;hostname;pwd
 module load singularity
 
 # For debug purpose, check if CUDA is currently available for torch. If available, will return `True`.
-singularity exec --nv /blue/vendor-nvidia/hju/monaicore0.8.1 python3 -c "import torch; print(torch.cuda.is_available())"
+singularity exec --nv /blue/vendor-nvidia/hju/monaicore0.9.1 python3 -c "import torch; print(torch.cuda.is_available())"
 
 # Run a tutorial python script within the container. Modify the path to your container and your script.
-singularity exec --nv /blue/vendor-nvidia/hju/monaicore0.8.1 python3 /home/hju/tutorials/2d_segmentation/torch/unet_training_array.py
+singularity exec --nv /blue/vendor-nvidia/hju/monaicore0.9.1 python3 /home/hju/tutorials/2d_segmentation/torch/unet_training_array.py
