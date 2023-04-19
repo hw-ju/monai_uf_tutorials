fe66f241e1741490ac14cc0349e63cfbf2cc7d4f
hw-ju <hju@nvidia.com>
2023-04-19 01:25:08 -0400

    list.sh 2

--- AI4HEALTH_workshop/label/list.sh
+++ AI4HEALTH_workshop/label/list.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=4
-#SBATCH --mem-per-cpu=64gb
-#SBATCH --partition=gpu
-#SBATCH --gpus=a100:1
-#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+#SBATCH --mem-per-cpu=10gb
+#SBATCH --time=1:00:00
 #SBATCH --output=%x.%j.out
 
 date;hostname;pwd
